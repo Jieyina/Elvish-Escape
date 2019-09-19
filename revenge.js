da998caa2191eff717ba66099fcc0f4c2c37189a
Jieyina <nasjieyi@gmail.com>
2019-09-19 02:01:07 -0600

    end screen, revenge countdown, slash change

--- revenge.js
+++ revenge.js
@@ -19,13 +19,27 @@ Revenge.prototype = {
         this.revenge.scale.setTo(0.6,0.6);
         this.revenge.animations.add("fire", [0, 1, 2, 3, 4], 15, true);
         this.revenge.play("fire");
+        this.countDown = this.game.add.text(250, 400, "", { fontSize: "48px", fill: "#fff" });
+        this.countStart = this.time.time;
     },
 
     update: function() {
         this.game.time.events.add(6000, this.startRevenge, this, null);
+        if (this.time.time > this.countStart + 1000 && this.time.time < this.countStart + 2000) {
+            this.countDown.text = "5";
+        } else if (this.time.time > this.countStart + 2000 && this.time.time < this.countStart + 3000) {
+            this.countDown.text = "4";
+        } else if (this.time.time > this.countStart + 3000 && this.time.time < this.countStart + 4000) {
+            this.countDown.text = "3";
+        } else if (this.time.time > this.countStart + 4000 && this.time.time < this.countStart + 5000) {
+            this.countDown.text = "2";
+        } else if (this.time.time > this.countStart + 5000 && this.time.time < this.countStart + 6000) {
+            this.countDown.text = "1";
+        }
     },
 
     startRevenge: function(){
+        this.countDown.text = "";
         this.game.state.start("Bonus", true, false, this.score, this.result);
     }
 };
